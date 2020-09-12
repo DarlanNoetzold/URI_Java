6cddb61376f8df6bd1b7cd40f53daec2a93c5537
Darlan Noetzold <41628589+DarlanNoetzold@users.noreply.github.com>
2020-09-12 09:29:10 -0300

    .

--- src/URI_1065.java
+++ src/URI_1065.java
@@ -2,16 +2,17 @@ import java.util.Scanner;
 
 public class URI_1065 {
 	public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in);
-        int contador = 0;
+        try (Scanner entrada = new Scanner(System.in)) {
+			int contador = 0;
 
-        for (int i = 1; i <= 5; i++) {
-            int num = entrada.nextInt();
-            if (num % 2 == 0) {
-                contador += 1;
-            }
-        }
-        System.out.printf("%d valores pares\n", contador);
+			for (int i = 1; i <= 5; i++) {
+			    int num = entrada.nextInt();
+			    if (num % 2 == 0) {
+			        contador += 1;
+			    }
+			}
+			System.out.printf("%d valores pares\n", contador);
+		}
 
     }
 }
